132aed1a01c44b864e73efe7eea9d3351de72f28
Dhaerya21 <dhaeryakhanna01@gmail.com>
2025-03-05 00:34:01 +0530

    completed basic structure of the program

---
+++ hangman_answers.py
@@ -0,0 +1,23 @@
+# answer for hangman game 
+hangman_answers = {
+    "animals": [
+        "elephant", "giraffe", "dolphin", "kangaroo", "penguin", "tiger", "cheetah", "crocodile", "hippopotamus", "rhinoceros",
+        "squirrel", "octopus", "jaguar", "koala", "chameleon", "ostrich", "peacock", "wolf", "zebra", "alpaca"
+    ],
+    "plants": [
+        "rose", "tulip", "daffodil", "orchid", "sunflower", "bamboo", "cactus", "fern", "ivy", "mangrove",
+        "maple", "oak", "pine", "willow", "lavender", "basil", "thyme", "aloe", "mushroom", "eucalyptus"
+    ],
+    "fruits": [
+        "apple", "banana", "cherry", "grape", "orange", "pineapple", "strawberry", "blueberry", "kiwi", "mango",
+        "peach", "plum", "pomegranate", "raspberry", "watermelon", "coconut", "fig", "guava", "papaya", "lychee"
+    ],
+    "countries": [
+        "canada", "brazil", "argentina", "germany", "france", "italy", "spain", "portugal", "china", "japan",
+        "india", "russia", "australia", "mexico", "southafrica", "egypt", "turkey", "greece", "sweden", "norway"
+    ],
+    "languages": [
+        "english", "spanish", "french", "german", "italian", "portuguese", "russian", "chinese", "japanese", "korean",
+        "hindi", "arabic", "turkish", "dutch", "swedish", "greek", "hebrew", "bengali", "thai", "vietnamese"
+    ]
+}
